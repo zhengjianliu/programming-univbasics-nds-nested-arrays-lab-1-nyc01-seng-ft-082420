bd2f0b5a7646ed53027c6ea42b86259844d57759
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 15:40:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,8 +36,7 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-  array = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
-  array
+  matrix[row][colum]
 end
 
 def matrix_update(matrix, row, column, new_value)
